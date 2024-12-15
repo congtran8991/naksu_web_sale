240caabed19ff1dcd6e5e64645df26555304707b
congtran8991 <congtran8991@gmail.com>
2024-12-15 10:16:13 +0700

    change variable first_name to first_name1

--- src/app/page1/page.tsx
+++ src/app/page1/page.tsx
@@ -27,7 +27,7 @@ export default function Dashboard() {
     },
   });
 
-  const [firstName] = useWatch({
+  const [firstName1] = useWatch({
     control: methods.control,
     name: ['firstName'],
   });
